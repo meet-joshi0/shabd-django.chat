ef193bd9d43935531a1934f70edc153aa1828313
meet joshi <meetjoshi1352@gmail.com>
2021-08-17 18:16:04 +0530

    fix database connection error

--- chat/settings.py
+++ chat/settings.py
@@ -109,8 +109,8 @@ DATABASES = {
 
      }
  }
- 
-db_from_env = dj_database_url.config(conn_max_age=500)
+
+db_from_env = dj_database_url.config(conn_max_age=500, ssl_require=True)
 DATABASES['default'].update(db_from_env)
 
 
