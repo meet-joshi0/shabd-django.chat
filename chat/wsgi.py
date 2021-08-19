91c11ecf14e777fcd1925474b7bdae6270d098bc
meet joshi <meetjoshi1352@gmail.com>
2021-08-19 22:39:00 +0530

    reconfigure whitenoice package

--- chat/wsgi.py
+++ chat/wsgi.py
@@ -8,17 +8,8 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
-
-
-from pathlib import Path
 from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat.settings')
 
 application = get_wsgi_application()
-BASE_DIR = Path(__file__).resolve().parent.parent
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
-application = WhiteNoise(application, root=STATIC_ROOT)
-#application.add_files('/path/to/more/static/files', prefix='more-files/')
