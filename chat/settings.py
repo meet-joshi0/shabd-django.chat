b268310b4e655a0307264a895d04829881c057fa
meet joshi <meetjoshi1352@gmail.com>
2021-08-20 01:57:38 +0530

    change default storage to cloudinary

--- chat/settings.py
+++ chat/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATE_DIR = os.path.join(BASE_DIR, "chat", "templates")
 
 MEDIA_URL = '/media/'
-#MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
