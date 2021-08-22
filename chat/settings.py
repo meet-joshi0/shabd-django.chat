4291276c66cd5225bdf5d222a05bfa1a647234f7
meet joshi <meetjoshi1352@gmail.com>
2021-08-22 19:37:21 +0530

    fix password for redis url

--- chat/settings.py
+++ chat/settings.py
@@ -140,7 +140,7 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'chat', 'static'), )
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-redis_db_url ='redis://'+config('redis_password')+'@redis-18556.c14.us-east-1-2.ec2.cloud.redislabs.com:18556'
+redis_db_url ='redis://'+config('redis_password')+'@redis-18556.c14.us-east-1-2.ec2.cloud.redislabs.com/18556'
 print("redis uel  ....> ", redis_db_url)
 CHANNEL_LAYERS = {
     'default': {
