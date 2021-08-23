7e1053d048c803c42be2566ee372e573706197e8
meet joshi <meetjoshi1352@gmail.com>
2021-08-23 16:28:14 +0530

    fix redis connection error

--- shabd/routing.py
+++ shabd/routing.py
@@ -3,7 +3,7 @@ from . import consumers
 
 websocket_urlpatterns = [
    # re_path(r'ws/chat/(?P<room_name>\w+)/$',consumers.GroupChatConsumer.as_asgi()),
-    path('ws/chat/<str:room_name>/',consumers.GroupChatConsumer.as_asgi()),
+    path('ws/groupchat/<str:room_name>/',consumers.GroupChatConsumer.as_asgi()),
     path('ws/userchat/<str:user_name1>/<str:user_name2>/',consumers.UserChatConsumer.as_asgi()),
     path('ws/notification/<str:username>/',consumers.ChatNotification.as_asgi()),
     path('ws/activeusers/<str:room_name>/',consumers.ActiveGroupUsers.as_asgi()),
