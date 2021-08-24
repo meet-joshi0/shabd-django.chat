36e023d4112176a8bf0fd01e6cd45e9353c00924
meet joshi <meetjoshi1352@gmail.com>
2021-08-24 21:19:01 +0530

    fix redis connection error

--- shabd/consumers.py
+++ shabd/consumers.py
@@ -28,20 +28,20 @@ class GroupChatConsumer(AsyncWebsocketConsumer):
 
         user_name = str(self.scope['user'])
         username_group = "new_user_%s" % (user_name)
-        add_online_user = await userList(self, user_name)
-        user_data = await userInformationList(self, user_name)
+        # add_online_user = await userList(self, user_name)
+        # user_data = await userInformationList(self, user_name)
 
 
     
         # send userdata to NewActiveGroupUser consumer
-        await self.channel_layer.group_send(
-            username_group,
-            {
-                "type": "chat_message",
-                "message":  user_data,
+        # await self.channel_layer.group_send(
+        #     username_group,
+        #     {
+        #         "type": "chat_message",
+        #         "message":  user_data,
 
-            }
-        )
+        #     }
+        # )
 
         await self.channel_layer.group_add(
             self.room_group_name, self.channel_name
@@ -408,7 +408,7 @@ def userList(self, name):
     user_list = r.sadd(self.room_group_name, name)
     return user_list
 
-# find reciver name given two name where username is logged in usersname 
+# find reciver name given two name where username is logged  usersname 
 def reciver_name(username, user1, user2):
     
     reciver = ""
