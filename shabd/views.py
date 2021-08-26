39d7c98f14398b16435eef1194c3b24d0daedc9c
meet joshi <meetjoshi1352@gmail.com>
2021-08-27 00:48:44 +0530

    fix create chat group permission issue in grouplist view

--- shabd/views.py
+++ shabd/views.py
@@ -53,7 +53,7 @@ class groupList(CreateView):
 
     def form_valid(self, form):
         if self.request.user.is_authenticated:
-            return super(GroupForm,self).form_valid(form)
+            return super(groupList,self).form_valid(form)
         else:
             return redirect('login')
 
